ca170ea27a6b7f33aba1ddf14485e746978c7fee
victor nwafor <victorokwy29@gmail.com>
2023-02-16 03:32:21 -0800

    Task 0

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,9 +1,3 @@
-guillaume@ubuntu:~/$ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
-Enter password:
-Database
-hbtn_0c_0
-information_schema
-mysql
-performance_schema
-sys
-guillaume@ubuntu:~/$
+-- Displays all databases
+-- Query to display databases
+SHOW DATABASES;
--- 0x0D-SQL_introduction/README.md
+++ 0x0D-SQL_introduction/README.md
@@ -1,1 +1,2 @@
 a script that lists all databases of your MySQL server
+This README is for 0-list_databases.sql
