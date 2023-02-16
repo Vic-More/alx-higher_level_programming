fcfcd2ec006bed45ab086350f70cd11ae8058008
victor nwafor <victorokwy29@gmail.com>
2023-02-16 02:39:16 -0800

    0-list_databases.sql

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,9 @@
+guillaume@ubuntu:~/$ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+Enter password:
+Database
+hbtn_0c_0
+information_schema
+mysql
+performance_schema
+sys
+guillaume@ubuntu:~/$
